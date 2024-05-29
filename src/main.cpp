3a2106dc7644522fbd324fcfc4f3c76e33d6642d
Alp <alpefeka2001@gmail.com>
2024-05-29 11:40:38 -0400

    Implemened keyword lexical analysis Currently understands the 'return' statement, blank space, semicolon, and numbers.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cctype>
 
 int main(int argc, char* argv[]) {
 	// error checking for correct usage
@@ -9,14 +10,82 @@ int main(int argc, char* argv[]) {
 		std::cerr<<"helium <file_name>.he"<<std::endl;
 		return -1;
 	}
+	/*
+	enum keyword_t {RETURN, IF, UNKNOWN};
+	struct keyword_list_t {std::string keyword, enum keyword_t id} keyword_list[]{
+		{"return", RETURN},
+		{"if", IF}
+	}
+	*/
+	
+
 	// read the file into string file_text
 	std::string file_text;
 	std::ifstream ifs(argv[1]);
 	file_text.assign((std::istreambuf_iterator<char>(ifs)),
 			(std::istreambuf_iterator<char>()	));
+
+	// Lexical analysis
+	std::string buffer;
+	for (int i = 0; i < file_text.length(); i++){
+		/************************************/
+		// TODO:
+		// > Make the logic cleaner
+		// > include other keywords
+		// > include a lot more things
+		/************************************/
+		// alphabetical value followed by alphanumeric value
+		// e.g. return, uint64_t, etc.
+		if(std::isalpha(file_text.at(i))){	
+			buffer.push_back(file_text.at(i));
+			i++;
+			while (std::isalnum(file_text.at(i))){
+				buffer.push_back(file_text.at(i));
+				i++;			
+			}
+			i--;
+		}
+		// number value
+		else if(std::isdigit(file_text.at(i))){
+			buffer.push_back(file_text.at(i));
+			i++;
+			while (std::isdigit(file_text.at(i))){
+				buffer.push_back(file_text.at(i));
+				i++;			
+			}
+			i--;		
+			continue;
+		}
+		// blank line
+		// check for the value of buffer
+		else if (std::isblank(file_text.at(i)) || file_text.at(i) == ';'){
+			if (buffer == "return"){
+				//TODO:implement
+				std::cout<<"Return statement found!"<<std::endl;
+			}
+			else{
+				std::cerr<<"You messed up! Current buffer is..."<<std::endl;
+				std::cerr<<buffer<<std::endl;		
+			}
+			buffer.clear();
+		}
+		// punctuation
+		/*
+		else if (std::ispunct(file_text.at(i))){
+			//TODO: implement
+			if(buffer == ";"){
+				std::cout<<"Semicolon found!"<<std::endl;
+			}
+			buffer.clear();
+			
+		}
+		*/
+
+	}
 	
+			
 	// print file for now
-	std::cout<<file_text<<std::endl;
+	// std::cout<<file_text<<std::endl;
 
 	return 0;
 }
